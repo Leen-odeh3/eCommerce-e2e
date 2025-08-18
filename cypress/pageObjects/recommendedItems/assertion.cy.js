be947aef95df54e21f5b05df334bbb17d1135c64
Leen-odeh12 <leenodeh287@gmail.com>
2025-08-18 23:41:39 +0300

    Test Case 22: Add to cart from Recommended items

---
+++ cypress/pageObjects/recommendedItems/assertion.cy.js
@@ -0,0 +1,12 @@
+class RecommendedAssertions {
+
+  verifyRecommendedSection() {
+    cy.contains('h2', 'recommended items').should('be.visible');
+  }
+
+  verifyProductInCart() {
+    cy.get('.cart_description').should('exist').and('be.visible');
+  }
+}
+
+export default RecommendedAssertions;
